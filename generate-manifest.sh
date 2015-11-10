1b5228cb78adf4437a753064d231ae524231710d
Kei YAMAZAKI <daydream.yamazaki@gmail.com>
2015-11-10 14:05:42 +0900

    cf v223 + diego v0.1440.0

--- generate-manifest.sh
+++ generate-manifest.sh
@@ -4,10 +4,13 @@ base=$(realpath $(dirname $0))
 repo_base=$1
 
 cd ${repo_base}/cf-release
-./scripts/generate_deployment_manifest warden \
+./scripts/generate_deployment_manifest \
+    warden \
+    bosh-lite/stubs/dev-security-groups.yml \
+    bosh-lite/stubs/property_overrides.yml \
     ${base}/director.yml \
-    ${repo_base}/diego-release/stubs-for-cf-release/enable_consul_with_cf.yml \
-    ${repo_base}/diego-release/stubs-for-cf-release/enable_diego_ssh_in_cc.yml \
+    templates/cf-minimal-dev.yml \
+    ${repo_base}/diego-release/stubs-for-cf-release/enable_diego_ssh_in_cf.yml \
     > ${base}/cf.yml
 
 
